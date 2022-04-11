c504b28bcfd7c968199ee69cbe64161b373efa6c
Mukhammad Zhalolov <muhammadzalolov@gmail.com>
2022-04-11 23:42:52 +0700

    промежуточный коммит, но не работает singup

--- authentication/views.py
+++ authentication/views.py
@@ -1,16 +1,17 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from  django.contrib.auth.models import User
+from django.contrib.auth.models import User
 from django.contrib import messages
-# Create your views here.
+from SignApp import settings
+from django.core.mail import send_mail
+
 
 def home(request):
     return render(request, "authentication/index.html")
 
 
 def signup(request):
-
     if request.method == "POST":
         # username = request.POST.get('username')
         username = request.POST['username']
@@ -24,11 +25,17 @@ def signup(request):
             messages.error(request, "Username already exist! Please try some other username")
             return redirect('home')
         if User.objets.filter(email=email):
-            messages.error(request, "Email already registered! Please try some other username")
+            messages.error(request, "Email already registered!")
             return redirect('home')
+        if len(username) > 10:
+            messages.error(request, "Username must be under 10 characters")
 
+        if pass1 != pass2:
+            messages.error(request, "Passwords didn't match!")
 
-
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric!")
+            return redirect('home')
 
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
@@ -37,6 +44,15 @@ def signup(request):
         myuser.save()
 
         messages.success(request, 'Your Account has been successfully created.')
+
+        # Welcome Email
+
+        subject = 'Welcome to SignApp - Django login!!'
+        message = 'Hello ' + myuser.first_name + '!! \n' + 'Welcome to SignApp!! \n Thanks you visiting our website \n We have  also sent you a confirmation email, please confirm you email address in oder to activate your account. \n\n Thanking You'
+        from_email = settings.EMAIL_HOST_USER
+        to_list = [myuser.email]
+        send_mail(subject, message, from_email, to_list, fail_silently=True)
+
 
         return redirect('signin')
 
